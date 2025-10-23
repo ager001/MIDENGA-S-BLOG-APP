d388b3a30eac8ac10b74751b79cca09945a27107
ager001 <152964009+ager001@users.noreply.github.com>
2025-10-23 21:44:22 +0300

    created the header for my blog project

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar.jsx'
+import Header from '../components/Header.jsx'
+const Home = () => {
+  return (
+    <div>
+    <NavBar />
+    <Header/>
+    </div>
+  )
+}
+
+export default Home
