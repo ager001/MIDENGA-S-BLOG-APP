65b9192ace70033d61565d4d1af5dc822798d99f
ager001 <152964009+ager001@users.noreply.github.com>
2025-10-25 22:11:10 +0300

    I have created the backend of my blog and downloaded necessary dependencies

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js';
+import adminRouter from './routes/adminRoutes.js';
+
+const app = express();
+{/*- Purpose: Creates an instance of an Express application.
+- Why: This app object is used to define routes, middleware, and server behavior
+ */}
+
+ await connectDB();
+
+//Middlewares
+app.use(cors())
+{/*- Purpose: Applies the CORS middleware globally.
+- Why: Ensures all incoming requests from other origins are allowed (unless restricted).
+ */}
+app.use(express.json())
+{/*- Purpose: Parses incoming JSON payloads.
+- Why: Allows your API to handle application/json requests and access req.body.
+ */}
+
+
+ //Routes
+app.get('/', (req, res)=> res.send('API is Working'))
+app.use('/api/admin', adminRouter )
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+      console.log('Server is running on port' + PORT)
+})
+
+export default app;
