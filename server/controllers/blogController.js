e0b8d094ed73554dffd69e0f1bb6ebf284613a96
ager001 <152964009+ager001@users.noreply.github.com>
2025-10-26 13:47:26 +0300

    created an Api to Add new blog

---
+++ server/controllers/blogController.js
@@ -0,0 +1,46 @@
+import fs from 'fs'
+import imagekit from '../configs/imageKit.js';
+import Blog from '../models/Blog.js'
+
+
+export const addBlog = async (req, res)=>{
+    try {
+        const {title, subTitle, description, category, isPublished} = 
+        JSON.parse(req.body.blog);
+        const imageFile = req.file;
+        
+        //Check if all fields are present
+        if(!title || !description || !category || !imageFile){
+             return res.json ({success: false, message:"Missing required fields" })
+
+        }
+               const fileBuffer = fs.readFileSync(imageFile.path);
+               
+               //upload Image to ImageKit
+
+               const response = await imagekit.upload({
+                file: fileBuffer,
+                fileName: imageFile.originalname,
+                folder: "/blogs"
+               })
+                
+               //optimization through imagekit URL transformation
+
+               const optimizedImageUrl = imagekit.url({
+                       path: response.filePath,
+                       transformation: [
+                        {quality:'auto'},// Auto compression
+                        {format:'webp'}, // Convert to modern forma
+                        {width:'1280'} // width resizing
+                       ]
+               });
+
+               const image = optimizedImageUrl;
+
+               await Blog.create({title, subTitle, description, category, image, isPublished})
+               res.json({success: true, message: "Blog added successfully"})
+
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import 'dotenv/config'
 import cors from 'cors'
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
+
+
 
 const app = express();
 {/*- Purpose: Creates an instance of an Express application.
@@ -25,6 +28,7 @@ app.use(express.json())
  //Routes
 app.get('/', (req, res)=> res.send('API is Working'))
 app.use('/api/admin', adminRouter )
+app.use('/api/blog', blogRouter )
 
 const PORT = process.env.PORT || 3000;
 
